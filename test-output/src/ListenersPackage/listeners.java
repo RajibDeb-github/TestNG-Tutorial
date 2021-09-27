293e53497faa606ea13bebea3a6de4e2d6239fd2
Rajib <rajib.deb1991@gmail.com>
2021-09-27 15:08:46 +0530

    Hello

--- test-output/src/ListenersPackage/listeners.java
+++ test-output/src/ListenersPackage/listeners.java
@@ -4,7 +4,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class listeners implements ITestListener{
+public class listeners implements ITestListener{  
 
 	@Override
 	public void onFinish(ITestContext arg0) {
